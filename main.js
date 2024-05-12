5e6db9b93d03dfdd587afbb37e22bdfa8880382f
malaika khaliq lodhi <163471373+malaikakhaliqlodhi@users.noreply.github.com>
2024-05-12 08:57:13 -0700

    push my BMI calculatior on github

---
+++ main.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env Node
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+    {
+        type: "numbers",
+        name: "weightinkg",
+        message: "enter your weight in kg"
+    }, {
+        type: "numbers",
+        name: "heightinmeters",
+        message: "enter your height in meters"
+    },
+]);
+let BMI = answers.weightinkg / (answers.heightinmeters * answers.heightinmeters);
+console.log("your BMI is:", BMI);
